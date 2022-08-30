60e1d0d53cf8cb101895b114e204eb32dd564fb5
fvera-pp <fernando.veralopez@pod-point.com>
2022-08-30 12:26:11 +0200

    add migration

---
+++ typeorm-migrations/migrations/1661855164630-newMigration3.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class newMigration31661855164630 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
